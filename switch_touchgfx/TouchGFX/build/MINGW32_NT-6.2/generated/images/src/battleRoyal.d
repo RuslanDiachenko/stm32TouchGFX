b79640a5cf3ee49884787a5b1a52b96ffff9c67a
RuslanDiachenko <rus.diachenko@gmail.com>
2019-04-24 10:37:06 +0300

    modified gitignore, removed build files

--- switch_touchgfx/TouchGFX/build/MINGW32_NT-6.2/generated/images/src/battleRoyal.d
+++
@@ -1,5 +0,0 @@
-build/MINGW32_NT-6.2/generated/images/src/battleRoyal.o: \
- generated/images/src/battleRoyal.cpp \
- ../Middlewares/ST/TouchGFX/touchgfx///framework/include/touchgfx/hal/Config.hpp
-
-../Middlewares/ST/TouchGFX/touchgfx///framework/include/touchgfx/hal/Config.hpp:
